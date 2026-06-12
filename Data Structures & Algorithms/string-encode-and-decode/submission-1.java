bcab7cad466f800093ac0a9518e8876fd8647465
Punit Patel <patelpunit10@gmail.com>
2026-06-11 23:42:27 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String encode(List<String> strs) {
+        String encoded_string;
+        StringBuffer sb = new StringBuffer();
+
+        for (String ss : strs) {
+            int tempCount = ss.length();
+            char tempC = '#';
+            sb.append(tempCount);
+            sb.append(tempC);
+            sb.append(ss);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> decoded_strs = new ArrayList<>();
+
+        int tempIndex = 0;
+
+        while (tempIndex < str.length()) {
+            int delimiterIndex = str.indexOf('#', tempIndex);
+            int tempSize = Integer.parseInt(str.substring(tempIndex, delimiterIndex));
+
+            String s = str.substring(delimiterIndex + 1, delimiterIndex + 1 + tempSize);
+            decoded_strs.add(s);
+
+            tempIndex = delimiterIndex + 1 + tempSize;
+        }
+
+        return decoded_strs;
+    }
+}
